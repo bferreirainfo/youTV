cf0ade4f8062b1b9f714125b24d304762a120bd3
bfs <bfs@trancoso.pitang.intranet>
2014-08-04 20:17:47 -0300

    configuração da api do soundclound rodando :)

---
+++ youTV/src/main/java/business/SoundCloudApiTest.java
@@ -0,0 +1,20 @@
+package business;
+
+import java.util.ArrayList;
+
+import de.voidplus.soundcloud.SoundCloud;
+import de.voidplus.soundcloud.Track;
+
+public class SoundCloudApiTest {
+    //https://apigee.com/embed/console/soundcloud test endPoints
+    public static void main(String[] args) {
+        SoundCloud soundcloud =
+                new SoundCloud("4396efca860e926e3b214b8fc15e5ebc",
+                        "3fd42ccfaa15b351acc804ae3be79034", "bferreira.info@gmail.com", "thunder");
+
+        ArrayList<Track> result = soundcloud.findTrack("angra");
+        for (Track track : result) {
+            System.out.println("music id: " + track.getId());
+        }
+    }
+}
